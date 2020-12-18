70f2c8b0ed4a969e975f3fa793c5feb768106187
juho0428@naver.com <juho0428@naver.com>
2020-12-18 11:26:46 +0900

    checkout connect mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://flwn5:ironside1@node-react.njajl.mongodb.net/node-react?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => { console.log('MongDB Connected...') }).catch( (err) => console.log(err) );
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
