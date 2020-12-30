275976dcef67ade0012b1d1374c173da9291a8c1
juho0428@naver.com <juho0428@naver.com>
2020-12-31 01:08:48 +0900

    checkout logout

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import axios from 'axios';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
@@ -19,22 +19,35 @@ function App() {
           <Route exact path="/landing"  component = { LandingPage } />
 
           <Route exact path="/login" component = { LoginPage } />
-            
-          <Route exact path="/register" component = { RegisterPage }/>
 
-            
+          <Route exact path="/register" component = { RegisterPage }/>             
         </Switch>
     </Router>
   );
 }
 
-function Home() {
+function Home(props) {
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+          .then(res => {
+              if (res.data.success) {
+                  props.history.push('/login');
+              } else {
+                  alert('Fail Logout');
+              }
+          });
+  }
+
   return (
     <div style = {{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
     }}>
         Home Page
+        <button onClick={onClickHandler}>
+            로그아웃
+        </button>
     </div>
   )
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -42,9 +42,9 @@ function RegisterPage(props) {
         dispatch(registerUser(body))
             .then(res => {
                 if(res.payload.success) {
-                    props.history.push('/');
+                    props.history.push('/login');
                 } else {
-                    alert('Error');
+                    alert('Fail register');
                 }
             });
     }
